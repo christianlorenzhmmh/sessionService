4c7d9cf566c7ef40a0ce28ff57f08374bd457435
Christian Lorenz <christian.lorenz@hmmh.de>
2017-07-27 17:36:59 +0200

    fertig

--- src/main/java/de/bonprix/sessionservice/SesssionServiceController.java
+++ src/main/java/de/bonprix/sessionservice/SesssionServiceController.java
@@ -2,6 +2,7 @@ package de.bonprix.sessionservice;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -10,9 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,48 +30,90 @@ public class SesssionServiceController {
 	//	@RequestMapping(value="/{prettyUrl:[a-z0-9\\-]+}/")
 	//    public ModelAndView getChannel(@PathVariable("prettyUrl") String prettyUrl,
 
+//ggf. garnicht notwendig jedes property update zu schicken weil am ende des requests die komplette session gesaved wird
+//	@RequestMapping(value="/{sessionkey:[a-z0-9\\-]+}" , 
+//			method = RequestMethod.PUT)
+//	public HttpStatus setSessionValue(@PathVariable("sessionkey") String sessionkey,
+//			@RequestParam(value="name", required=true) String name,
+//			@RequestParam(value="value", required=true) String newValue) {
+//
+//		ShopSession shopSession = shopSessions.get(sessionkey);
+//		if (shopSession == null) {
+//						return HttpStatus.NOT_FOUND;
+////			shopSession = new ShopSession(sessionkey);
+////			shopSessions.put(sessionkey, shopSession);
+//		}
+//		shopSession.set(name, newValue);
+//		return HttpStatus.OK;
+//	}
 
-	@RequestMapping(value="/{sessionkey:[a-z0-9\\-]+}/" , method = RequestMethod.PUT)
-	public HttpStatus setSessionValue(@PathVariable("sessionkey") String sessionkey,
-			@RequestParam(value="name", required=true) String name,
-			@RequestParam(value="value", required=true) String newValue) {
+	
+	@RequestMapping(value="/" , 
+			method = RequestMethod.PUT, 
+			consumes=MediaType.APPLICATION_JSON_VALUE)
+	public HttpStatus saveSession(
+			@RequestBody ShopSession updatedShopSession) {
 
-		ShopSession shopSession = shopSessions.get(sessionkey);
-		if (shopSession == null) {
-			//			return HttpStatus.NOT_FOUND;
-			shopSession = new ShopSession(sessionkey);
-			shopSessions.put(sessionkey, shopSession);
-		}
-		shopSession.set(name, newValue);
+		shopSessions.put(updatedShopSession.getId(), updatedShopSession);
 		return HttpStatus.OK;
 	}
 
 
 
-
-//	@RequestMapping(value="/{sessionkey:[0-9\\-]+}/" , 
-//			method = RequestMethod.GET, 
-//			produces =  MediaType.APPLICATION_JSON_VALUE)
-//
-//	public @ResponseBody ResponseEntity<ShopSession> getSession(@PathVariable("sessionkey") String sessionkey) {
-//		ShopSession shopSession = shopSessions.get(sessionkey);
-//		if (shopSession == null) {
-//			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-//		}
-//		return ResponseEntity.ok(shopSession);
-//	}
-	
-	
-	@RequestMapping(value="/{sessionkey:[0-9\\-]+}/" , 
+	@RequestMapping(value="/{id:[a-z0-9\\-]+}" , 
 			method = RequestMethod.GET, 
 			produces =  MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody 
-	public ResponseEntity<ShopSession> getSession(@PathVariable("sessionkey") String sessionkey) {
-		ShopSession shopSession = shopSessions.get(sessionkey);
+
+	public @ResponseBody ResponseEntity<ShopSession> getSession(@PathVariable("id") String id) {
+		ShopSession shopSession = shopSessions.get(id);
 		if (shopSession == null) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 		}
 		return ResponseEntity.ok(shopSession);
+	}
+	
+	
+//	@RequestMapping(value="/{decodedWebshopSessionId:[0-9\\-]+}" , 
+//			method = RequestMethod.GET, 
+//			produces =  MediaType.APPLICATION_JSON_VALUE)
+//	@ResponseBody 
+//	public ResponseEntity<ShopSession> getSession(@PathVariable("decodedWebshopSessionId") String decodedWebshopSessionId) {
+//		Iterator<ShopSession> it = shopSessions.values().iterator();
+//		while (it.hasNext()) {
+//			ShopSession ss = it.next();
+//			if (decodedWebshopSessionId.equals(ss.getDecodedWebshopSessionId())) {
+//				return ResponseEntity.ok(ss);
+//			}
+//		}
+////		ShopSession shopSession = shopSessions.get(decodedWebshopSessionId);
+////		if (shopSession == null) {
+//			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+////		}
+//	}
+	
+	
+	//webshop muss sessionkey liefern (aber der sessionService erstellt seine unique "DB-ID" selber! sessionkey ist dann ein property der ShopSession)
+	//sessionKey wird zumindest erstmal die wkorbid sein
+//	@RequestMapping(value="/{sessionkey:[a-z0-9\\-]+}", 
+	@RequestMapping(value="/", 
+			method = RequestMethod.POST, 
+			produces =  MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody 
+	public ResponseEntity<ShopSession> createSession() {
+//	public ResponseEntity<ShopSession> createSession(@PathVariable("sessionkey") String sessionkey) {
+//	public HttpStatus createSession(@PathVariable("sessionkey") String sessionkey) {
+		String sessionId = createUniqueSessionId();
+		ShopSession shopSession = new ShopSession(sessionId);
+		shopSessions.put(sessionId, shopSession);
+		return ResponseEntity.ok(shopSession);
+		//			return HttpStatus.CREATED;
+		}
+
+
+
+	private String createUniqueSessionId() {
+		UUID id = UUID.randomUUID();
+		return "" + id.toString();
 	}
 
 
--- src/main/java/de/bonprix/sessionservice/ShopSession.java
+++ src/main/java/de/bonprix/sessionservice/ShopSession.java
@@ -1,84 +1,125 @@
 package de.bonprix.sessionservice;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ShopSession {
-	
-	String id;
-	Map<String, String> internal = new HashMap<>();
-	
-	
+
+	String id;//just for internal persistence 
+	private String decodedWebshopSessionId;
+	//	String sessionkey; //the wkorbid
+	//	Map<String, String> internal = new HashMap<>();
+
+
+    private long creationTime = System.currentTimeMillis();
+    
+    private long lastAccessedTime = creationTime;
+    
+    
+    
 	public ShopSession() {};
 	public ShopSession(String id) {
 		this.id = id;
 	}
 
-	
+
 	//1.tomcat-session-attribut:
 	//public static final String XPLOSION_NEWSLETTER_FLAG = "xplosionNewsletterFlag";
-//	String xplosionNewsletterFlag;
-	
-	//2.cookie
-//	String uid;
-	
-	//3.wkorb-wert
-//	String referrer;
-//
-//	public Long getLocalUniqueKey() {
-//		return localUniqueKey;
-//	}
+	String xplosionNewsletterFlag;
 
-	
+	//2.cookie
+	String uid;
+
+	//3.wkorb-wert
+	String useragent;
+	//
+	//	public Long getLocalUniqueKey() {
+	//		return localUniqueKey;
+	//	}
+
+
 
 	public void setId(String id) {
 		this.id = id;
 	}
-//
-//	public String getXplosionNewsletterFlag() {
-//		return xplosionNewsletterFlag;
-//	}
-//
-//	public void setXplosionNewsletterFlag(String xplosionNewsletterFlag) {
-//		this.xplosionNewsletterFlag = xplosionNewsletterFlag;
-//	}
-//
-//	public String getUid() {
-//		return uid;
-//	}
-//
-//	public void setUid(String uid) {
-//		this.uid = uid;
-//	}
-//
-//	public String getReferrer() {
-//		return referrer;
-//	}
-//
-//	public void setReferrer(String referrer) {
-//		this.referrer = referrer;
-//	}
-	
-	
-	public void set(String key, String value) {
-		internal.put(key, value);
-	}
-	
-	public String get(String key) {
-		return internal.get(key);
-	}
 	public String getId() {
 		return id;
 	}
-	public Map<String, String> getInternal() {
-		return internal;
+
+
+
+	//	public String getSessionkey() {
+	//		return sessionkey;
+	//	}
+	//	public void setSessionkey(String sessionkey) {
+	//		this.sessionkey = sessionkey;
+	//	}
+
+	public String getDecodedWebshopSessionId() {
+		return decodedWebshopSessionId;
+	}
+	public void setDecodedWebshopSessionId(String decodedWebshopSessionId) {
+		this.decodedWebshopSessionId = decodedWebshopSessionId;
+	}
+
+
+
+	public String getXplosionNewsletterFlag() {
+		return xplosionNewsletterFlag;
+	}
+
+	public void setXplosionNewsletterFlag(String xplosionNewsletterFlag) {
+		this.xplosionNewsletterFlag = xplosionNewsletterFlag;
+	}
+
+
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+
+	public String getUseragent() {
+		return useragent;
+	}
+	public void setUseragent(String useragent) {
+		this.useragent = useragent;
 	}
 	
 	
+	public long getCreationTime() {
+		return creationTime;
+	}
+	public void setCreationTime(long creationTime) {
+		this.creationTime = creationTime;
+	}
 	
+	
+	public long getLastAccessedTime() {
+		return lastAccessedTime;
+	}
+	public void setLastAccessedTime(long lastAccessedTime) {
+		this.lastAccessedTime = lastAccessedTime;
+	}
+
+
+	//	
+	//	public void set(String key, String value) {
+	//		internal.put(key, value);
+	//	}
+	//	
+	//	public String get(String key) {
+	//		return internal.get(key);
+	//	}
+
+	//	public Map<String, String> getInternal() {
+	//		return internal;
+	//	}
+	//	
+
+
 
 }
